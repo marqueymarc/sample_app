d86e5b52e2151286be1fdc4108ed7a58775894da
marqueymarc <marc@marcmeyer.com>
2011-05-25 18:04:07 -0700

    end ch5

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,29 +2,18 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   describe "GET /layout_links" do
-    @a = %w[/ /contact /about /help /signup]
-    @b = %w[Home Contact About Help Sign\ Up]
+    @a = %w[/contact /about /help / /signup]
+    @b = %w[Contact About Help Home Sign\ Up]
     @a.zip(@b).each do |url, name| 
       it "should have a #{name} page at #{url}" do
 	  get url
 	  response.should have_selector('title', :content => name)
       end
+      it "should have a #{name} link in the layout" do
+	visit root_path
+	click_link name
+	response.should have_selector('title', :content => name)
+      end
     end
   end
-#  describe "more layout links" do
-#    it "should have the right links on the layout" do
-#        visit root_path
-#        click_link "About"
-#        response.should have_selector('title', :content => "About")
-#        click_link "Help"
-#        response.should have_selector('title', :content => "Help")
-#        click_link "Contact"
-#        response.should have_selector('title', :content => "Contact")
-#	visit root_path
-#        click_link "Home"
-#        response.should have_selector('title', :content => "Home")
-#        click_link "Sign up now!"
-#        response.should have_selector('title', :content => "Sign up")
-#     end
-#   end
 end
