95c516b0c98829fba583bd8f248ea5615f86d0eb
marqueymarc <marc@marcmeyer.com>
2011-05-26 23:44:55 -0700

    added password and test structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,25 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+    attr_accessor :password
+    attr_accessible :name, :email, :password, :password_confirmation
 
-  [:name, :email].each do | sym |
-    validates sym, :presence => true, :length => {:maximum => 50 }
-  end
-  validates :email, :format => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-    :uniqueness=> {:case_sensitive => false}
+    [:name, :email].each do | sym |
+	validates sym, :presence => true, :length => {:maximum => 50 }
+    end
+    validates :email, :format => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+	:uniqueness=> {:case_sensitive => false}
+    validates :password, :presence=> true, 
+	:confirmation => true, 
+	:allow_blank => false,
+	:length => { :within => 6..40, :too_long => "password too long",
+	    :too_short => "password must be > 6 characters"}
+    before_save :encrypt_password
+    private 
+	def encrypt_password
+	    self.encrypted_password = encrypt(password)
+	end
+	def encrypt(s)
+	    s # for now
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe User do
+
+    def pp(s) 
+	{:password => s, :password_confirm => s}
+    end
+
     before(:each) do
-	@attr = {:name => "Example A. User", :email => "ex@user.com" }
+	@attr = {:name => "Example A. User", :email => "ex@user.com",
+	    :password => "lemmein", :password_confirm => "lemmein"}
 	@bad_mails = %w[marc@marc, marc_at_marc.com, marc@marc.]
     end
     
     it "should create a new instance given a valid attribute" do
-	User.create!(@attr)
+	u = User.create!(@attr)
     end
     [:name, :email].each do | sym |
       it "should require a #{sym}" do
@@ -30,5 +36,24 @@ describe User do
 	User.create!(@attr)
 	b_m = User.new(@attr.merge(:email => @attr[:email].upcase))
 	b_m.should_not be_valid
+    end
+    describe "Password validations" do
+	[ ["should have a password", ""] ,
+	  ["should fail with a password which is too short" , "aaaa"],
+	  ["should fail with a password which is too short", "a"*41]
+	].each do | pair |
+	    it pair[0] do
+		b_m = User.new(@attr.merge(pp pair[1]))
+		b_m.should_not be_valid
+	    end
+	end
+	it "should have an encrypted_password attribute" do
+	    u = User.create(@attr)
+	    u.should respond_to(:encrypted_password)
+	end
+	it "should set the encrypted password" do
+	    u = User.create(@attr)
+	    u.encrypted_password.should_not be_blank
+	end
     end
 end
