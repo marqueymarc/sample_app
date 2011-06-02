8b7818f9bb88ff1596bd059dad7a10e7675ca862
marqueymarc <marc@marcmeyer.com>
2011-06-01 19:05:23 -0700

    sigin 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 SampleApp::Application.routes.draw do
-  match '/contact', :to => "pages#contact"
-  match '/about', :to => "pages#about"
-  match '/help', :to => "pages#help"
-  match '/signup', :to => "users#new"
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup', :to => "users#new"
+  match "/signin", :to => "sessions#new"
+  match '/contact', :to => "pages#contact"
+
+  match '/about', :to => "pages#about"
+  match '/help', :to => "pages#help"
+  match "/signout", :to => "sessions#destroy"
+
 
   root :to => "pages#home"
 
